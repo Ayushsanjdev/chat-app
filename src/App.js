7b090ef27d26a90c47442499a99bd6f8f84d13b6
Ayush Sanj <ayushsanjpro@gmail.com>
2021-05-05 22:24:08 +0530

    remove: text-align add: indent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBSOd8tuoKqaC9z3Qrmw8zqAm0_HVLYwiw",
+  authDomain: "jibber-jabber-3e686.firebaseapp.com",
+  projectId: "jibber-jabber-3e686",
+  storageBucket: "jibber-jabber-3e686.appspot.com",
+  messagingSenderId: "145479449441",
+  appId: "1:145479449441:web:c29b4c2498e5618201e320"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 function App() {
+
+  const [user] = useAuthState(auth); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+
+  function SignIn() {
+    const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+    return (
+      <button onClick={signInWithGoogle}>Sign in with google</button>
+    )
+  }
+
+  function SignOut() {
+    return auth.currentUser && (
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    )
+  }
+
+  function ChatRoom() {
+
+    const messageRef = firestore.collection('messages');
+    const query = messageRef.orderBy('create At').limit(25);
+
+    const [messages] = useCollectionData(query, {idField: 'id'});
+
+    return (
+      <div>
+        <div>
+          {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+
+  function ChatMessage(props) {
+    const {text, uid} = props.message;
+
+    return <p>text is: { text }</p>
+  }
 }
 
 export default App;
