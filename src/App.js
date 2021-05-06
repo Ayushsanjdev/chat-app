9a9b97ab6875c87405380fe1d9b2c6b48d474b72
Ayush Sanj <ayushsanjpro@gmail.com>
2021-05-06 15:41:58 +0530

    add attribute to emonji

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {useRef, useState} from 'react';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
   apiKey: "AIzaSyBSOd8tuoKqaC9z3Qrmw8zqAm0_HVLYwiw",
@@ -15,108 +15,117 @@ firebase.initializeApp({
   storageBucket: "jibber-jabber-3e686.appspot.com",
   messagingSenderId: "145479449441",
   appId: "1:145479449441:web:c29b4c2498e5618201e320"
-})
+});
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-
 function App() {
-
-  const [user] = useAuthState(auth); 
+  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <header>
-        <h1>Jabber Jibber 📳</h1>
-        <SignOut/>
+        <h1>
+          Jabber Jibber{" "}
+          <span role="img" aria-label="accessible">
+            📳
+          </span>
+        </h1>
+        <SignOut />
       </header>
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
 
-  function SignIn() {
-    const signInWithGoogle = () => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      auth.signInWithPopup(provider);
-    }
-    return (
-      <>
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return (
+    <>
       <button onClick={signInWithGoogle}>Sign in with google</button>
       <p>No violation and copyright !</p>
-      </>
-    )
-  }
+    </>
+  );
+}
 
-  function SignOut() {
-    return auth.currentUser && (
-      <button onClick={() => auth.signOut()}>Sign Out</button>
-    )
-  }
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
 
-  function ChatRoom() {
+function ChatRoom() {
+  const dummy = useRef();
 
-    const dummy = useRef();
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
 
-    const messagesRef = firestore.collection('messages');
-    const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
 
-    const [messages] = useCollectionData(query, {idField: 'id'});
+  const [formValue, setFormValue] = useState("");
 
-    const [formValue, setFormValue] = useState('');
+  const sendMessage = async (e) => {
+    e.preventDefault();
 
-    const sendMessage = async (e) => {
-      e.preventDefault();
-      
-      const { uid, photoURL } = auth.currentUser;
+    const { uid, photoURL } = auth.currentUser;
 
-      await messagesRef.add({
-        text: formValue,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        uid,
-        photoURL
-      })
-      
-      setFormValue('');
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
 
-      dummy.current.scrollIntoView({ behavior: 'smooth' });
-    }
+    setFormValue("");
 
-    return (
-      <>
-        <main>
-          {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-          <span ref={dummy}>
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
 
-          </span>
+        <span ref={dummy}></span>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="type something... "
+        />
 
-        </main>
-        <form onSubmit={sendMessage}>
+        <button type="submit" disabled={!formValue}>
+          Send
+        </button>
+      </form>
+    </>
+  );
+}
 
-          <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="type something... " />
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
 
-          <button type="submit" disabled={!formValue}>Send</button>
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
-        </form>
-      </>
-    )
-  }
-
-  function ChatMessage(props) {
-    const {text, uid, photoURL } = props.message;
-
-    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
-    return (<>
+  return (
+    <>
       <div className={`message ${messageClass}`}>
-        <img alt='profile pic' src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png' } />
+        <img
+          alt="profile pic"
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+        />
         <p>{text}</p>
       </div>
-    </>)
-  }
+    </>
+  );
+}
 
 export default App;
